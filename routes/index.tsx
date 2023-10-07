f0e094a7c62ac2eaba309d027b4ffc0f97a94508
nikogoli <k_m_naoto@yahoo.co.jp>
2023-10-07 15:11:36 +0900

    Change： index.tsx の差し替え

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,14 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { asset } from "$fresh/runtime.ts"
+//import { Toaster } from "./components/ui/toaster"
+import App from "../islands/App.tsx"
 
-export default function Home() {
-  const count = useSignal(3);
+
+export default function Home(){
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+    <div>
+      <App />
+      {/*<Toaster />*/}
+      <link href={asset("/uno.css")} rel="stylesheet" />
     </div>
-  );
+  )
 }
