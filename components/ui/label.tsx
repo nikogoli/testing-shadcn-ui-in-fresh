816dae559cf36732468eb3a93218982cb7eb3027
nikogoli <k_m_naoto@yahoo.co.jp>
2023-10-08 16:39:40 +0900

    Change：Label を利用可能に

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react'
+import * as React from 'preact/compat'
 import * as LabelPrimitive from '@radix-ui/react-label'
+import * as AltLabelPrimitive from "../../lib/components/label.d.ts"
 import { VariantProps, cva } from 'class-variance-authority'
 
-import { cn } from '../../lib/utils'
+import { cn } from '../../lib/utils.ts'
+import { ElementRef, ComponentPropsWithoutRef } from "../../lib/type-utils.ts"
 
 const labelVariants = cva('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70')
 
 const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
+  ElementRef<typeof AltLabelPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AltLabelPrimitive.Root> & VariantProps<typeof labelVariants>
+>(({ class:className, ...props }, ref) => (
   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
 ))
 Label.displayName = LabelPrimitive.Root.displayName
