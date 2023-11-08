c33c9923acf239f9ece62b7d5aa006091bf2b072
nikogoli <k_m_naoto@yahoo.co.jp>
2023-11-08 11:42:48 +0900

    Fix?：checkbox のオリジナルの class への言及を追加
    [チェック記号の色が変えられない問題](https://github.com/nikogoli/testing-shadcn-ui-in-fresh/issues/6) 関連

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -13,6 +13,7 @@ const Checkbox = React.forwardRef<
     ref={ref}
     className={cn(
       'peer h-4 w-4 shrink-0 rounded-sm border border-input ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary',
+      // original is not 'data-[state=checked]:border-primary' but 'data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground', but somehow text color does not change.
       className
     )}
     {...props}
