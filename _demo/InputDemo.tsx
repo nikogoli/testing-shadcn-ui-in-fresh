79dc515b1e00c8d224d2797f9315875f78d7e9da
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-10 23:12:38 +0900

    追加：Input のデモ

---
+++ _demo/InputDemo.tsx
@@ -0,0 +1,19 @@
+import { Input } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.5/components/input.tsx"
+
+import DemoWrapper from "./_DemoWrapper.tsx"
+
+
+const Code = `
+import { Input } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.5/components/input.tsx"
+
+export function InputDemo() {
+  return <Input type="email" placeholder="Email" class="w-90% max-w-450px" />
+}
+`
+
+export function InputDemo() {
+  return(
+    <DemoWrapper code_text={Code.trim()}>
+      <Input type="email" placeholder="Email" class="w-90% max-w-450px" />
+    </DemoWrapper>
+)}
