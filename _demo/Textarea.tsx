8f3e2f459f06261c0199758bd2d6e4827f1087f2
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-10 23:13:23 +0900

    追加：TextArea のデモ

--- _demo/Textarea.tsx
+++
@@ -1,5 +0,0 @@
-import { Textarea } from '../components/ui/textarea.tsx'
-
-export function TextareaDemo() {
-  return <Textarea placeholder="Type your message here." class='!w-100' />
-}
---
+++ _demo/TextareaDemo.tsx
@@ -0,0 +1,21 @@
+import { Textarea } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.5/components/textarea.tsx'
+
+import DemoWrapper from "./_DemoWrapper.tsx"
+
+
+const Code = `
+import { Textarea } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.5/components/textarea.tsx'
+
+export function TextareaDemo() {
+  return <Textarea placeholder="Type your message here." class='!w-100' />
+}
+`
+
+
+export function TextareaDemo() {
+  return (
+    <DemoWrapper code_text={Code.trim()}>
+      <Textarea placeholder="Type your message here." class='!w-100' />
+    </DemoWrapper>
+  
+)}
