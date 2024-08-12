4c596d9fe6c2c29bc2003822687701d252754d7a
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-12 11:58:59 +0900

    アップデート：Tooltip デモ

--- _demo/TooltipDemo.tsx
+++ _demo/TooltipDemo.tsx
@@ -1,14 +1,50 @@
 import { Button } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/button.tsx'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/tooltip.tsx'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/tooltip.tsx'
 
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 const Code = `
-`
+import { Button } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/button.tsx'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/tool
 
 export function TooltipDemo() {
   return (
-    <DemoWrapper code_text={Code.trim()}>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="outline" class="w-10 rounded-full p-0">
+            <span class="i-lucide:plus flex h-4 w-4" />
+            <span class="sr-only">Add</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+`
+
+const info = {
+  title: "Tooltip",
+  text: "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."
+}
+
+
+export function TooltipDemo() {
+  return (
+    <DemoWrapper code_text={Code.trim()} info={info} >
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger asChild>
