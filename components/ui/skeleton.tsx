53d7ea3d7c5d08d8975a597f5013333bdb2d1dcb
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-12 12:07:05 +0900

    不要になった components 系フォルダの削除

--- components/ui/skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from '../../lib/utils.ts'
-
-function Skeleton({ class:className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />
-}
-
-export { Skeleton }
