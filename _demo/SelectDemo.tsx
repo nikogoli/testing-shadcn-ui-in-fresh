830784c2ca6437d350e2faab891880c5710a65ed
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-12 11:57:35 +0900

    アップデート：Select デモ

--- _demo/SelectDemo.tsx
+++ _demo/SelectDemo.tsx
@@ -21,11 +21,36 @@ import {
   SelectTrigger,
   SelectValue,
 } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/select.tsx"
-`
 
 export function SelectDemo() {
   return (
-    <DemoWrapper code_text={Code.trim()}>
+    <Select>
+      <SelectTrigger class="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+`
+
+const info = {
+  title: "Select",
+  text: "Displays a list of options for the user to pick from—triggered by a button."
+}
+
+export function SelectDemo() {
+  return (
+    <DemoWrapper code_text={Code.trim()} info={info} >
       <Select>
         <SelectTrigger class="w-[180px]">
           <SelectValue placeholder="Select a fruit" />
