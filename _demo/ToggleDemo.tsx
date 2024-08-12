ffbfa4f987ce3d154e71bd5efca61b7addccfa79
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-12 11:58:43 +0900

    アップデート：Toggle デモ

--- _demo/ToggleDemo.tsx
+++ _demo/ToggleDemo.tsx
@@ -3,12 +3,27 @@ import { Toggle } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/com
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 const Code = `
+import { Toggle } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/toggle.tsx'
+
+export function ToggleDemo() {
+  return (
+    <Toggle aria-label="Toggle italic">
+      <span class="i-lucide:bold flex h-4 w-4" />
+    </Toggle>
+  )
+}
+
 `
+
+const info = {
+  title: "Toggle",
+  text: "A two-state button that can be either on or off."
+}
 
 
 export function ToggleDemo() {
   return (
-    <DemoWrapper code_text={Code.trim()}>
+    <DemoWrapper code_text={Code.trim()} info={info} >
       <Toggle aria-label="Toggle italic">
         <span class="i-lucide:bold flex h-4 w-4" />
       </Toggle>
