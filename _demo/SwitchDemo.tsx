dc930c0fc17f519bca466e86c6ad777224e39008
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-12 11:58:06 +0900

    アップデート：Switch デモ

--- _demo/SwitchDemo.tsx
+++ _demo/SwitchDemo.tsx
@@ -5,16 +5,32 @@ import DemoWrapper from "./_DemoWrapper.tsx"
 
 
 const Code = `
-`
+import { Label } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/label.tsx"
+import { Switch } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.6/components/switch.tsx"
 
 export function SwitchDemo() {
   return (
-    <DemoWrapper code_text={Code.trim()}>
+    <div class="flex items-center space-x-2">
+      <Switch id="airplane-mode" />
+      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    </div>
+  )
+}
+`
+
+const info = {
+  title: "Switch",
+  text: "A control that allows the user to toggle between checked and not checked."
+}
+
+
+export function SwitchDemo() {
+  return (
+    <DemoWrapper code_text={Code.trim()} info={info} >
       <div class="flex items-center space-x-2">
         <Switch id="airplane-mode" />
         <Label htmlFor="airplane-mode">Airplane Mode</Label>
       </div>
     </DemoWrapper>
-    
   )
 }
