077058ad3f66d256180bb616ac9c3fa49ece2e7f
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-18 20:38:41 +0900

    追加(tw config)：色の設定 + CSSの読み込みを削除

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,11 +6,8 @@ export default function Home(){
   return (
     <div>
       <App />
-      <link href={asset("/uno.css")} rel="stylesheet" />
-      <link href={asset("/shadcn.css")} rel="stylesheet" />
       <link href={asset("/default.css")} rel="stylesheet" />
       <link href={asset("/visual-studio-dark.css")} rel="stylesheet" />
-      <style> {`button:focus { outline-style: none !important}`} </style>
     </div>
   )
 }
--- twind.config.ts
+++ twind.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig, Preset } from "twind";
+import * as colors from "https://esm.sh/@twind/preset-tailwind@1.1.4/colors"
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 
 export default {
   ...defineConfig({
     presets: [presetTailwind() as Preset, presetAutoprefix()],
+    theme:{
+      colors:{
+        ...colors,
+        background:'hsl(var(--background) / <alpha-value>)',
+        foreground:'hsl(var(--foreground) / <alpha-value>)',
+        destructive: 'hsl(var(--destructive) / <alpha-value>)'
+      }
+    }
   }),
   selfURL: import.meta.url,
 };
