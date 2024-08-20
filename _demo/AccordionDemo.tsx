60f5d216ede825538c0773d395e0c8f9328089a2
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-20 12:06:25 +0900

    変更(Acrdデモ)：微調整

--- _demo/AccordionDemo.tsx
+++ _demo/AccordionDemo.tsx
@@ -1,6 +1,12 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.0.9/components/accordion.tsx'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '../../testing_shadcn_ui_for_deno/components/accordion.tsx'
 
 import DemoWrapper from "./_DemoWrapper.tsx"
+
 
 const Code = `
 import {
@@ -12,44 +18,43 @@ import {
 
 export function AccordionDemo() {
   return (
-    <Accordion type="single" collapsible class="w-[500px]">
+    <Accordion type="single" collapsible class="w-[500px]" >
       <AccordionItem value="item-1">
-        <AccordionTrigger>{"Is it accessible?"}</AccordionTrigger>
-        <AccordionContent>
-          {"Yes. It adheres to the WAI-ARIA design pattern."}
-        </AccordionContent>
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger>{"Is it styled?"}</AccordionTrigger>
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
         <AccordionContent>
-          {"Yes. It comes with default styles that matches the other components' aesthetic."}
+          Yes. It comes with default styles that matches the other components' aesthetic.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>{"Is it animated?"}</AccordionTrigger>
-        <AccordionContent>
-          {"Yes. It's animated by default, but you can disable it if you prefer."}
-        </AccordionContent>
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
       </AccordionItem>
     </Accordion>
   )
 }`
+
 
 const info = {
   title: "Accordion",
   text: "A vertically stacked set of interactive headings that each reveal a section of content."
 }
 
+
 const has_error = {
   type: "minor" as const,
   text: "The closing-animation is not played.\nThe opening-animation is not played smoothly."
 }
 
+
 export function AccordionDemo() {
   return (
     <DemoWrapper code_text={Code.trim()} info={info} has_error={has_error}
                 preview_h={280} place_class="justify-center">
-      <Accordion type="single" collapsible class="w-[500px]">
+      <Accordion type="single" collapsible class="w-[500px]" >
         <AccordionItem value="item-1">
           <AccordionTrigger>Is it accessible?</AccordionTrigger>
           <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
