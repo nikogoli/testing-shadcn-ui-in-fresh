edeba3b351747290a0bd9fdb2734b5b18e8b22bf
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-20 12:03:38 +0900

    追加(tw config)：色設定

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, Preset } from "twind";
-import * as colors from "https://esm.sh/@twind/preset-tailwind@1.1.4/colors"
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 
@@ -7,13 +6,48 @@ export default {
   ...defineConfig({
     presets: [presetTailwind() as Preset, presetAutoprefix()],
     theme:{
-      colors:{
-        ...colors,
-        background:'hsl(var(--background) / <alpha-value>)',
-        foreground:'hsl(var(--foreground) / <alpha-value>)',
-        destructive: 'hsl(var(--destructive) / <alpha-value>)'
-      },
       extend: {
+        colors:{
+          border: "hsl(var(--border) / <alpha-value>)",
+          input: "hsl(var(--input) / <alpha-value>)",
+          ring: "hsl(var(--ring) / <alpha-value>)",
+          background: "hsl(var(--background) / <alpha-value>)",
+          foreground: "hsl(var(--foreground) / <alpha-value>)",
+          primary: {
+            DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+            foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+          },
+          secondary: {
+            DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+            foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+          },
+          destructive: {
+            DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+            foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+          },
+          muted: {
+            DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+            foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+          },
+          accent: {
+            DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+            foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
+          },
+          popover: {
+            DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+            foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
+          },
+          card: {
+            DEFAULT: "hsl(var(--card) / <alpha-value>)",
+            foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+          },
+        },
+        borderRadius: {
+          xl: "calc(var(--radius) + 4px)",
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
         keyframes: {
           "accordion-down": {
             '0%': { height: '0' },
