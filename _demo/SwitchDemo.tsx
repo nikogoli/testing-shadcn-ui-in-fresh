f87d2dd82b9755bc516ad36efd1caff1a4f11547
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-22 19:11:47 +0900

    変更(Switchデモ)：disable のデモを追加

--- _demo/SwitchDemo.tsx
+++ _demo/SwitchDemo.tsx
@@ -1,5 +1,5 @@
-import { Label } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.9/components/label.tsx"
-import { Switch } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.9/components/switch.tsx"
+import { Label } from "../../testing_shadcn_ui_for_deno/components/label.tsx"
+import { Switch } from "../../testing_shadcn_ui_for_deno/components/switch.tsx"
 
 import DemoWrapper from "./_DemoWrapper.tsx"
 
@@ -10,9 +10,15 @@ import { Switch } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.9/com
 
 export function SwitchDemo() {
   return (
-    <div class="flex items-center space-x-2">
-      <Switch id="airplane-mode" />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    <div class="flex flex-col gap-4">
+      <div class="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
+      <div class="flex items-center space-x-2">
+        <Switch id="bluetooth-tethering" disabled />
+        <Label htmlFor="bluetooth-tethering">Bluetooth Tethering</Label>
+      </div>
     </div>
   )
 }
@@ -27,9 +33,15 @@ const info = {
 export function SwitchDemo() {
   return (
     <DemoWrapper code_text={Code.trim()} info={info} >
-      <div class="flex items-center space-x-2">
-        <Switch id="airplane-mode" />
-        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      <div class="flex flex-col gap-4">
+        <div class="flex items-center space-x-2">
+          <Switch id="airplane-mode" />
+          <Label htmlFor="airplane-mode">Airplane Mode</Label>
+        </div>
+        <div class="flex items-center space-x-2">
+          <Switch id="bluetooth-tethering" disabled />
+          <Label htmlFor="bluetooth-tethering">Bluetooth Tethering</Label>
+        </div>
       </div>
     </DemoWrapper>
   )
