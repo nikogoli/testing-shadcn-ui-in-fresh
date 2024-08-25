0d04158cd89a891cc5cdfa8a18016b9ce25221de
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 23:11:33 +0900

    追加(Sliderデモ)：数字表示を追加

--- _demo/SliderDemo.tsx
+++ _demo/SliderDemo.tsx
@@ -1,20 +1,29 @@
 import { Slider } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.9/components/slider.tsx"
+import { signal } from "@preact/signals"
 
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
 const Code = `
 import { Slider } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.0.9/components/slider.tsx"
+import { signal } from "@preact/signals"
 
 export function SliderDemo() {
+  const val_sig = signal(50)
   return (
-    <div class="w-[450px]">
-      <Slider
-        defaultValue={[50]}
-        max={100}
-        step={1}
-        class="w-full"
-      />
+    <div class="flex gap-4 items-center">
+      <div class="w-[450px]">
+        <Slider
+          defaultValue={[50]}
+          max={100}
+          step={1}
+          class="w-full"
+          onValueChange={val => val_sig.value = val[0]}
+        />
+      </div>
+      <div class="w-6 border-b-2 border-black">
+        <span class="text-xl font-bold">{val_sig}</span> 
+      </div>
     </div>
   )
 }
@@ -31,15 +40,22 @@ const has_error = {
 }
 
 export function SliderDemo() {
+  const val_sig = signal(50)
   return (
     <DemoWrapper code_text={Code.trim()} info={info} >
-      <div class="w-[450px]">
-        <Slider
-          defaultValue={[50]}
-          max={100}
-          step={1}
-          class="w-full"
-        />
+      <div class="flex gap-4 items-center">
+        <div class="w-[450px]">
+          <Slider
+            defaultValue={[50]}
+            max={100}
+            step={1}
+            class="w-full"
+            onValueChange={val => val_sig.value = val[0]}
+          />
+        </div>
+        <div class="w-6 border-b-2 border-black">
+          <span class="text-xl font-bold">{val_sig}</span> 
+        </div>
       </div>
     </DemoWrapper>
   )
