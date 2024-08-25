48182c97e8851099767342a9a52b811e0341f9af
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 13:03:33 +0900

    Update useHighLight.js

--- hooks/useHighLight.js
+++ hooks/useHighLight.js
@@ -1,19 +1,5 @@
 // from "https://zenn.dev/kstdx/articles/2874b355035d22"
 
-import css from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/css.js'
-import csv from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/csv.js'
-import html from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/html.js'
-import js from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/js.js'
-import js_template_literals from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/js_template_literals.js'
-import jsdoc from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/jsdoc.js'
-import json from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/json.js'
-import md from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/md.js'
-import plain from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/plain.js'
-import py from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/py.js'
-import ts from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/ts.js'
-import xml from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/xml.js'
-import yaml from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/yaml.js'
-
 const expandData = {
     num: {
         type: 'num',
@@ -29,20 +15,74 @@ const expandData = {
     }
 }
 
+/** from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/js.js' */
+const JavaScript = [
+	{
+		match: /\/\*\*((?!\*\/)[^])*(\*\/)?/g,
+		sub: 'jsdoc'
+	},
+	{
+		match: /\/\/.*\n?|\/\*((?!\*\/)[^])*(\*\/)?/g,
+		sub: 'todo'
+	},
+	{
+		expand: 'str'
+	},
+	{
+		match: /`((?!`)[^]|\\[^])*`?/g,
+		sub: 'js_template_literals'
+	},
+	{
+		type: 'kwd',
+		match: /=>|\b(this|set|get|as|async|await|break|case|catch|class|const|constructor|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|if|implements|import|in|instanceof|interface|let|var|of|new|package|private|protected|public|return|static|super|switch|throw|throws|try|typeof|void|while|with|yield)\b/g
+	},
+	{
+		match: /\/((?!\/)[^\r\n\\]|\\.)+\/[dgimsuy]*/g,
+		sub: 'regex'
+	},
+	{
+		expand: 'num'
+	},
+	{
+		type: 'num',
+		match: /\b(NaN|null|undefined|[A-Z][A-Z_]*)\b/g
+	},
+	{
+		type: 'bool',
+		match: /\b(true|false)\b/g
+	},
+	{
+		type: 'oper',
+		match: /[/*+:?&|%^~=!,<>.^-]+/g
+	},
+	{
+		type: 'class',
+		match: /\b[A-Z][\w_]*\b/g
+	},
+	{
+		type: 'func',
+		match: /[a-zA-Z$_][\w$_]*(?=\s*((\?\.)?\s*\(|=\s*(\(?[\w,{}\[\])]+\)? =>|function\b)))/g
+	}
+]
+
+
+/** from 'https://deno.land/x/speed_highlight_js@v1.2.6/src/languages/ts.js' */
+const TypeScript = [
+	{
+		type: 'type',
+		match: /:\s*(any|void|number|boolean|string|object|never|enum)\b/g
+	},
+	{
+		type: 'kwd',
+		match: /\b(type|namespace|typedef|interface|public|private|protected|implements|declare|abstract|readonly)\b/g
+	},
+	...JavaScript
+]
+
+
 const langs = {
-        css,
-        csv,
-        html,
-        js,
-        js_template_literals,
-        jsdoc,
-        json,
-        md,
-        plain,
-        py,
-        ts,
-        xml,
-        yaml
+        js: JavaScript,
+        ts: TypeScript,
     }
 
 const sanitize = (str = '') =>str.replaceAll('&', '&#38;')
