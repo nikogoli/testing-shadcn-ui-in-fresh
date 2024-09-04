652cdde424a31a61dad2e87bf9427b0c96f9cf51
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-04 21:31:15 +0900

    変更(DemoWrapper)：デモを遅延読み込みする形式に変更

--- _demo/_DemoWrapper.tsx
+++ _demo/_DemoWrapper.tsx
@@ -1,20 +1,26 @@
-import { ComponentProps } from "preact"
+import { ComponentProps, JSX } from "preact"
+import { Suspense, lazy } from 'preact/compat';
+import { useRef } from "preact/hooks"
 
 import WrapperAlert from "./_WrapperAlert.tsx"
 import { highlightText } from "../hooks/useHighLight.ts"
 
 
-
 export default function DemoWrapper(props:{
   code_text: string,
-  info?: {title:string, text:string},
+  info: {title:string, text:string},
+  funcCompForDemo: (()=>Promise<()=>JSX.Element>) | null,
   preview_h?: number,
   place_class?: string,
   has_error?: {type:"mejor"|"minor", text:string},
 } & ComponentProps<"div">) {
-  const { code_text, info, children, has_error } = props
+  const { code_text, info, funcCompForDemo, has_error } = props
   const preview_h = props.preview_h ?? 200
   const place_class = props.place_class ?? "place-content-center"
+  
+  const container_ref = useRef<HTMLDivElement>(null)
+  const Demo = funcCompForDemo ? lazy(funcCompForDemo) : () => (<></>)
+
   return (
     <div class="h-full w-full flex flex-col gap-8 p-6">
       <WrapperAlert has_error={has_error} />
@@ -23,8 +29,12 @@ export default function DemoWrapper(props:{
           <div class="text-2xl font-semibold">{info?.title ?? "Preview"}</div>
           <p class="text-gray-500">{info?.text ?? ""}</p>
         </div>
-        <div class={`h-[${preview_h}px] p-4 border rounded-xl grid ${place_class} relative`}>
-          {children}
+        <div class={`h-[${preview_h}px] p-4 border rounded-xl grid relative
+                     has-[div#spinner]:place-content-center ${place_class}`}
+              ref={container_ref} >
+          <Suspense fallback={<SimpleSpinner id="spinner" />}>
+            <Demo />
+          </Suspense>
         </div>
       </div>
       <div class="flex flex-col">
@@ -34,6 +44,24 @@ export default function DemoWrapper(props:{
               dangerouslySetInnerHTML={{__html:highlightText(code_text, "ts", false)}}>
         </code>
       </div>
+    </div>
+  )
+}
+
+
+function SimpleSpinner(props:{
+  size?: number,
+  main_color?: string,
+  bg_color?: string,
+}&ComponentProps<"div">){
+  const { size, main_color, bg_color, ...others } = props
+  const size_sty = size ? `h-${size} w-${size}` : "h-10 w-10"
+  const main = "border-" + (main_color ?? "sky-400")
+  const bg = bg_color ?? "inherit"
+  return (
+    <div class="flex justify-center" {...others} >
+      <div class={`animate-spin ${size_sty} border-4 ${main} rounded-full`}
+            style={{"border-top-color": bg}}></div>
     </div>
   )
 }
