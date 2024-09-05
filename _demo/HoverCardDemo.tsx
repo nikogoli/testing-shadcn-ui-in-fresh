67342cb4f4ffe14032e16caa5f477fc16ff05fb9
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:22:35 +0900

    変更(HvrCrd デモ)：遅延読み込み化

--- _demo/HoverCardDemo.tsx
+++ _demo/HoverCardDemo.tsx
@@ -1,16 +1,3 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage
-} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/avatar.tsx'
-import { Button } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx'
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger
-} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/hover-card.tsx'
-import IconCalendarMonth from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/calendar-month.tsx"
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -54,29 +41,49 @@ const info = {
 
 
 export function HoverCardDemo() {
-  return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <HoverCard>
-        <HoverCardTrigger asChild>
-          <Button variant="link">@nextjs</Button>
-        </HoverCardTrigger>
-        <HoverCardContent class="w-80">
-          <div class="flex justify-between space-x-4">
-            <Avatar>
-              <AvatarImage src="https://github.com/vercel.png" />
-              <AvatarFallback>VC</AvatarFallback>
-            </Avatar>
-            <div class="space-y-1">
-              <h4 class="text-sm font-semibold">@nextjs</h4>
-              <p class="text-sm">The React Framework – created and maintained by @vercel.</p>
-              <div class="flex items-center pt-2">
-                <IconCalendarMonth class="mr-2 h-4 w-4 opacity-70" />
-                <span class="text-xs text-muted-foreground">Joined December 2021</span>
+  const createComp = async () => {
+    const {
+      Avatar,
+      AvatarFallback,
+      AvatarImage
+    } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/avatar.tsx')
+    const { Button } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx')
+    const {
+      HoverCard,
+      HoverCardContent,
+      HoverCardTrigger
+    } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/hover-card.tsx')
+    const { default: IconCalendarMonth } = await import("https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/calendar-month.tsx")
+    
+    const Demo = () => {
+      return (
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Button variant="link">@nextjs</Button>
+          </HoverCardTrigger>
+          <HoverCardContent class="w-80">
+            <div class="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="https://github.com/vercel.png" />
+                <AvatarFallback>VC</AvatarFallback>
+              </Avatar>
+              <div class="space-y-1">
+                <h4 class="text-sm font-semibold">@nextjs</h4>
+                <p class="text-sm">The React Framework – created and maintained by @vercel.</p>
+                <div class="flex items-center pt-2">
+                  <IconCalendarMonth class="mr-2 h-4 w-4 opacity-70" />
+                  <span class="text-xs text-muted-foreground">Joined December 2021</span>
+                </div>
               </div>
             </div>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
-    </DemoWrapper>
+          </HoverCardContent>
+        </HoverCard>
+      )
+    }
+    return Demo
+  }
+
+  return (
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
