099d82d705a7eb68bd1b9fa12c44e7ea8ef6149a
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:22:11 +0900

    変更(Dilg デモ)：遅延読み込み化

--- _demo/DialogDemo.tsx
+++ _demo/DialogDemo.tsx
@@ -1,16 +1,3 @@
-import { Button } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/dialog.tsx'
-import { Input } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/input.tsx'
-import { Label } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/label.tsx'
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -69,36 +56,56 @@ const info = {
 
 
 export function DialogDemo() {
+  const createComp = async () => {
+    const { Button } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx')
+    const {
+      Dialog,
+      DialogContent,
+      DialogDescription,
+      DialogFooter,
+      DialogHeader,
+      DialogTitle,
+      DialogTrigger,
+    } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/dialog.tsx')
+    const { Input } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/input.tsx')
+    const { Label } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/label.tsx')
+
+    const Demo = () => {
+      return (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
+            </DialogHeader>
+            <div class="grid gap-4 py-4">
+              <div class="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" class="text-right">
+                  Name
+                </Label>
+                <Input id="name" value="Pedro Duarte" class="col-span-3" />
+              </div>
+              <div class="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" class="text-right">
+                  Username
+                </Label>
+                <Input id="username" value="@peduarte" class="col-span-3" />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )
+    }
+    return Demo
+  }
+  
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
-          </DialogHeader>
-          <div class="grid gap-4 py-4">
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" class="text-right">
-                Name
-              </Label>
-              <Input id="name" value="Pedro Duarte" class="col-span-3" />
-            </div>
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" class="text-right">
-                Username
-              </Label>
-              <Input id="username" value="@peduarte" class="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
