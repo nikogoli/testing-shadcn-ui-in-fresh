6492e928f85f7e619c5fd29c18092e986d9f3575
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:23:20 +0900

    変更(Nav デモ)：遅延読み込み化

--- _demo/NavigationMenuDemo.tsx
+++ _demo/NavigationMenuDemo.tsx
@@ -1,14 +1,4 @@
 import { ComponentProps } from "preact"
-
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/navigation-menu.tsx'
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -169,110 +159,122 @@ const components: { title: string; href: string; description: string }[] = [
 ]
 
 
-const FirstItem = () => (
-  <NavigationMenuItem>
-    <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-    <NavigationMenuContent>
-      <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-        <li class="row-span-3">
-          <NavigationMenuLink asChild>
-            <a
-              class="flex h-full w-full select-none flex-col justify-end rounded-md bg-accent/50 p-6 no-underline outline-none focus:shadow-md"
-              href="/"
-            >
-              <div class="mb-2 mt-4 text-lg font-medium">shadcn/ui</div>
-              <p class="text-sm leading-tight text-muted-foreground">
-                Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
-              </p>
-            </a>
-          </NavigationMenuLink>
-        </li>
-        <ListItem href="/docs" title="Introduction">
-          Re-usable components built using Radix UI and Tailwind CSS.
-        </ListItem>
-        <ListItem href="/docs/installation" title="Installation">
-          How to install dependencies and structure your app.
-        </ListItem>
-        <ListItem href="/docs/primitives/typography" title="Typography">
-          Styles for headings, paragraphs, lists...etc
-        </ListItem>
-      </ul>
-    </NavigationMenuContent>
-  </NavigationMenuItem>
-)
-
-
-const SecondItem = () => (
-  <NavigationMenuItem>
-    <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-    <NavigationMenuContent>
-      <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-        {components.map((component) => (
-          <li>
-            <NavigationMenuLink asChild>
-              <a class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none
-                       transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent
-                       focus:text-accent-foreground"
-                    key={component.title} href={component.href}>
-                <div class="text-sm font-medium leading-none">{component.title}</div>
-                <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">{component.description}</p>
-              </a>
-            </NavigationMenuLink>
-          </li>
-        ))}
-      </ul>
-    </NavigationMenuContent>
-  </NavigationMenuItem>
-)
-
-
-const ThirdItem = () => (
-  <NavigationMenuItem>
-    <NavigationMenuTrigger>Documentation</NavigationMenuTrigger>
-    <NavigationMenuContent>
-      <NavigationMenuLink asChild>
-        <ul class="grid w-[400px] h-[200px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-          <li>
-            <NavigationMenuLink asChild>
-              <a class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none
-                  transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent
-                  focus:text-accent-foreground">
-                <div class="font-medium leading-none">Documentation</div>
-              </a>
-            </NavigationMenuLink>
-          </li>
-        </ul>
-      </NavigationMenuLink>
-    </NavigationMenuContent>
-  </NavigationMenuItem>
-)
-
-
-function ListItem(props:{ title: string } & ComponentProps<"a">){
-  const { title, children, class:classStr, ...others } = props
-  return (
-  <li>
-    <NavigationMenuLink asChild>
-      <a class={`block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${classStr}`}
-        {...others}>
-        <div class="text-sm font-medium leading-none">{title}</div>
-        <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-      </a>
-    </NavigationMenuLink>
-  </li>
-)}
-
-
 export function NavigationMenuDemo() {
+  const createComp = async () => {
+    const {
+      NavigationMenu,
+      NavigationMenuContent,
+      NavigationMenuItem,
+      NavigationMenuLink,
+      NavigationMenuList,
+      NavigationMenuTrigger,
+    } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/navigation-menu.tsx')
+
+    const FirstItem = () => (
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+            <li class="row-span-3">
+              <NavigationMenuLink asChild>
+                <a
+                  class="flex h-full w-full select-none flex-col justify-end rounded-md bg-accent/50 p-6 no-underline outline-none focus:shadow-md"
+                  href="/"
+                >
+                  <div class="mb-2 mt-4 text-lg font-medium">shadcn/ui</div>
+                  <p class="text-sm leading-tight text-muted-foreground">
+                    Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
+                  </p>
+                </a>
+              </NavigationMenuLink>
+            </li>
+            <ListItem href="/docs" title="Introduction">
+              Re-usable components built using Radix UI and Tailwind CSS.
+            </ListItem>
+            <ListItem href="/docs/installation" title="Installation">
+              How to install dependencies and structure your app.
+            </ListItem>
+            <ListItem href="/docs/primitives/typography" title="Typography">
+              Styles for headings, paragraphs, lists...etc
+            </ListItem>
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    )
+
+    const SecondItem = () => (
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            {components.map((component) => (
+              <li>
+                <NavigationMenuLink asChild>
+                  <a class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none
+                           transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent
+                           focus:text-accent-foreground"
+                        key={component.title} href={component.href}>
+                    <div class="text-sm font-medium leading-none">{component.title}</div>
+                    <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">{component.description}</p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+            ))}
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    )
+
+    const ThirdItem = () => (
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Documentation</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <NavigationMenuLink asChild>
+            <ul class="grid w-[400px] h-[200px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              <li>
+                <NavigationMenuLink asChild>
+                  <a class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none
+                      transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent
+                      focus:text-accent-foreground">
+                    <div class="font-medium leading-none">Documentation</div>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuLink>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    )
+
+    function ListItem(props:{ title: string } & ComponentProps<"a">){
+      const { title, children, class:classStr, ...others } = props
+      return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a class={`block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${classStr}`}
+            {...others}>
+            <div class="text-sm font-medium leading-none">{title}</div>
+            <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    )}
+
+    const Demo = () => {
+      return (
+        <NavigationMenu>
+          <NavigationMenuList>
+            <FirstItem />
+            <SecondItem />
+            <ThirdItem />
+          </NavigationMenuList>
+        </NavigationMenu>
+      )
+    }
+    return Demo
+  }
+
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <NavigationMenu>
-        <NavigationMenuList>
-          <FirstItem />
-          <SecondItem />
-          <ThirdItem />
-        </NavigationMenuList>
-      </NavigationMenu>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
