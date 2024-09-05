58c3923799c0d25197eabd878fe3ef23ae14105f
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:25:12 +0900

    変更(Slidr デモ)：遅延読み込み化

--- _demo/SliderDemo.tsx
+++ _demo/SliderDemo.tsx
@@ -1,4 +1,3 @@
-import { Slider } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/slider.tsx"
 import { signal } from "@preact/signals"
 
 import DemoWrapper from "./_DemoWrapper.tsx"
@@ -34,29 +33,34 @@ const info = {
   text: "An input where the user selects a value from within a given range."
 }
 
-const has_error = {
-  type: "mejor" as const,
-  text: "The slider's Thumb does not apper."
-}
 
 export function SliderDemo() {
-  const val_sig = signal(50)
+  const createComp = async () => {
+    const { Slider } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/slider.tsx")
+
+    const Demo = () => {
+      const val_sig = signal(50)
+      return (
+        <div class="flex gap-4 items-center">
+          <div class="w-[450px]">
+            <Slider
+              defaultValue={[50]}
+              max={100}
+              step={1}
+              class="w-full"
+              onValueChange={val => val_sig.value = val[0]}
+            />
+          </div>
+          <div class="w-6 border-b-2 border-black">
+            <span class="text-xl font-bold">{val_sig}</span> 
+          </div>
+        </div>
+      )
+    }
+    return Demo
+  }
+
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <div class="flex gap-4 items-center">
-        <div class="w-[450px]">
-          <Slider
-            defaultValue={[50]}
-            max={100}
-            step={1}
-            class="w-full"
-            onValueChange={val => val_sig.value = val[0]}
-          />
-        </div>
-        <div class="w-6 border-b-2 border-black">
-          <span class="text-xl font-bold">{val_sig}</span> 
-        </div>
-      </div>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
