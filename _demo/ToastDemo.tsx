dd6453c3299554fedabec617e3c7b5ccb916f198
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:25:57 +0900

    変更(Tost デモ)：遅延読み込み化

--- _demo/ToastDemo.tsx
+++ _demo/ToastDemo.tsx
@@ -1,19 +1,12 @@
-import { VNode, JSX, Fragment } from "preact";
-import { Button } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx"
-import {
-  ToastAction,
-  Toaster,
-  useToast,
-  toastVariants
-} from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/toast.tsx"
+import { Fragment } from "preact"
 
-import DemoWrapper from "./_DemoWrapper.tsx";
+import DemoWrapper from "./_DemoWrapper.tsx"
 import WrapperSelect from "./_WrapperSelect.tsx"
 import { useState } from "preact/hooks"
 
 
 const Code = `
-import { VNode, JSX, Fragment } from "preact";
+import { Fragment } from "preact";
 import { Button } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx"
 import {
   ToastAction,
@@ -56,36 +49,50 @@ const info = {
 }
 
 
-function ToastDemoBase(porps:{variant:keyof typeof toastVariants["variants"]["variant"]}) {
-  const { toast } = useToast()
-  return (
-    <Button
-      variant={porps.variant == "default" ? "outline" : "destructive"}
-      onClick={() => {
-        toast({
-          title: "Scheduled: Catch up ",
-          description: "Friday, February 10, 2023 at 5:57 PM",
-          action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-        })
-      }}
-    >
-      Add to calendar
-    </Button>
-  )
-}
-
-
 export function ToastDemo(){
-  const defVari = toastVariants.defaultVariants.variant
-  const [actVariant, setVariant] = useState<keyof typeof toastVariants["variants"]["variant"]>(defVari)
-  const variNames = Object.keys(toastVariants.variants.variant) as Array<typeof toastVariants["defaultVariants"]["variant"]>
+  const createComp = async () => {
+    const { Button } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx")
+    const {
+      ToastAction,
+      Toaster,
+      useToast,
+      toastVariants
+    } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/toast.tsx")
+
+    function ToastDemoBase(porps:{variant:keyof typeof toastVariants["variants"]["variant"]}) {
+      const { toast } = useToast()
+      return (
+        <Button
+          variant={porps.variant == "default" ? "outline" : "destructive"}
+          onClick={() => {
+            toast({
+              title: "Scheduled: Catch up ",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+              action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+            })
+          }}
+        >
+          Add to calendar
+        </Button>
+      )
+    }
+
+    const Demo = () => {
+      const defVari = toastVariants.defaultVariants.variant
+      const [actVariant, setVariant] = useState<typeof toastVariants["defaultVariants"]["variant"]>(defVari)
+      const variNames = Object.keys(toastVariants.variants.variant) as Array<typeof toastVariants["defaultVariants"]["variant"]>
+      return (
+        <Fragment>
+          <WrapperSelect {...{setVariant, variNames, defVari}} />
+          <ToastDemoBase variant={actVariant} />
+          <Toaster variant={actVariant} />
+        </Fragment>
+      )
+    }
+    return Demo
+  }
+
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <WrapperSelect {...{setVariant, variNames, defVari}} />
-      <Fragment>
-        <ToastDemoBase variant={actVariant} />
-        <Toaster variant={actVariant} />
-      </Fragment>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
