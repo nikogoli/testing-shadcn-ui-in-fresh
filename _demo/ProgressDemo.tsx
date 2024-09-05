da1aee47008124cebe02c6598a83fcf812a6257f
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:24:00 +0900

    変更(Prgrs デモ)：遅延読み込み化

--- _demo/ProgressDemo.tsx
+++ _demo/ProgressDemo.tsx
@@ -1,10 +1,5 @@
-import { Progress } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/progress.tsx"
-import { useState, useEffect } from "preact/hooks"
-
-import { Button } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx"
-import IconReload from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/reload.tsx"
-
 import DemoWrapper from "./_DemoWrapper.tsx"
+import { useState, useEffect } from "preact/hooks"
 
 
 const Code = `
@@ -44,25 +39,36 @@ const info = {
 
 
 export function ProgressDemo() {
-  const [progress, setProgress] = useState(-3)
+  const createComp = async () => {
+    const { Progress } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/progress.tsx")
+    const { Button } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx")
+    const { default: IconReload } = await import("https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/reload.tsx")
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (progress < 100){ setProgress(prev => prev+1) }
-      else { clearTimeout(timer) }
-    }, 80)
-    return () => clearTimeout(timer)
-  }, [])
+    const Demo = () => {
+      const [progress, setProgress] = useState(-3)
+
+      useEffect(() => {
+        const timer = setInterval(() => {
+          if (progress < 100){ setProgress(prev => prev+1) }
+          else { clearTimeout(timer) }
+        }, 80)
+        return () => clearTimeout(timer)
+      }, [])
+  
+      return (
+        <div class="flex gap-6 items-center">
+          <Progress value={progress} class="w-80" />
+          <Button disabled={progress < 100} variant="outline" class="w-fit h-fit"
+                  onClick={_ev => setProgress(-3)} >
+            <IconReload class="w-4 h-4" />
+          </Button>
+        </div>
+      )
+    }
+    return Demo
+  }
 
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <div class="flex gap-6 items-center">
-        <Progress value={progress} class="w-80" />
-        <Button disabled={progress < 100} variant="outline" class="w-fit h-fit"
-                onClick={_ev => setProgress(-3)} >
-          <IconReload class="w-4 h-4" />
-        </Button>
-      </div>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
