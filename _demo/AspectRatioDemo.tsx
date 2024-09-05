00bdf32f53cfa4ef616b624c42621082676531d2
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:20:07 +0900

    変更(AspcRto デモ)：遅延読み込み化

--- _demo/AspectRatioDemo.tsx
+++ _demo/AspectRatioDemo.tsx
@@ -1,5 +1,3 @@
-import { AspectRatio } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/aspect-ratio.tsx"
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -28,17 +26,27 @@ const info = {
 
 
 export function AspectRatioDemo() {
+  const createComp = async () => {
+    const { AspectRatio } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/aspect-ratio.tsx")
+
+    const Demo = () => {
+      return (
+        <div class="w-[415px]">
+          <AspectRatio ratio={16 / 9} class="bg-muted">
+            <img
+              src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+              alt="Photo by Drew Beamer"
+              class="w-full h-full block rounded-md object-cover"
+            />
+          </AspectRatio>
+        </div>
+      )
+    }
+    return Demo
+  }
+
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} preview_h={300}>
-      <div class="w-[415px]">
-        <AspectRatio ratio={16 / 9} class="bg-muted">
-          <img
-            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-            alt="Photo by Drew Beamer"
-            class="w-full h-full block rounded-md object-cover"
-          />
-        </AspectRatio>
-      </div>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp}
+                  preview_h={300} />
   )
 }
