bf55a9d7aa1456e5ac7567ecf2a4bbeb00c3184c
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:26:14 +0900

    変更(TglGrpデモ)：遅延読み込み化

--- _demo/ToggleGroupDemo.tsx
+++ _demo/ToggleGroupDemo.tsx
@@ -1,12 +1,3 @@
-import IconBold from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/bold.tsx"
-import IconItalic from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/italic.tsx"
-import IconUnderline from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/underline.tsx"
-
-import {
-  ToggleGroup,
-  ToggleGroupItem,
-} from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/toggle-group.tsx"
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -45,19 +36,34 @@ const info = {
 
 
 export function ToggleGroupDemo() {
+  const createComp = async () => {
+    const { default: IconBold } = await import("https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/bold.tsx")
+    const { default: IconItalic } = await import("https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/italic.tsx")
+    const { default: IconUnderline } = await import("https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/underline.tsx")
+    const {
+      ToggleGroup,
+      ToggleGroupItem,
+    } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/toggle-group.tsx")
+
+    const Demo = () => {
+      return (
+        <ToggleGroup type="multiple">
+          <ToggleGroupItem value="bold" aria-label="Toggle bold">
+            <IconBold class="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="italic" aria-label="Toggle italic">
+            <IconItalic class="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="underline" aria-label="Toggle underline">
+            <IconUnderline class="h-4 w-4" />
+          </ToggleGroupItem>
+        </ToggleGroup>
+      )
+    }
+    return Demo
+  }
+  
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <ToggleGroup type="multiple">
-        <ToggleGroupItem value="bold" aria-label="Toggle bold">
-          <IconBold class="h-4 w-4" />
-        </ToggleGroupItem>
-        <ToggleGroupItem value="italic" aria-label="Toggle italic">
-          <IconItalic class="h-4 w-4" />
-        </ToggleGroupItem>
-        <ToggleGroupItem value="underline" aria-label="Toggle underline">
-          <IconUnderline class="h-4 w-4" />
-        </ToggleGroupItem>
-      </ToggleGroup>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
