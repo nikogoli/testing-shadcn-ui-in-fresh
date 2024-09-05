f456a3422b740c8125c0b44c1ac6b1621e025d66
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:25:01 +0900

    変更(Skeltnデモ)：遅延読み込み化

--- _demo/SkeletonDemo.tsx
+++ _demo/SkeletonDemo.tsx
@@ -1,5 +1,3 @@
-import { Skeleton } from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/skeleton.tsx'
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -26,15 +24,24 @@ const info = {
 
 
 export function SkeletonDemo() {
-  return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <div class="flex items-center space-x-4">
-        <Skeleton class="h-12 w-12 rounded-full" />
-        <div class="space-y-2">
-          <Skeleton class="h-4 w-[250px]" />
-          <Skeleton class="h-4 w-[200px]" />
+  const createComp = async () => {
+    const { Skeleton } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/skeleton.tsx')
+
+    const Demo = () => {
+      return (
+        <div class="flex items-center space-x-4">
+          <Skeleton class="h-12 w-12 rounded-full" />
+          <div class="space-y-2">
+            <Skeleton class="h-4 w-[250px]" />
+            <Skeleton class="h-4 w-[200px]" />
+          </div>
         </div>
-      </div>
-    </DemoWrapper>
+      )
+    }
+    return Demo
+  }
+
+  return (
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
