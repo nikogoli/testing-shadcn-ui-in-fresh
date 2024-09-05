4238771986a874cc126ee6aaa942eb81e48659be
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 21:21:40 +0900

    変更(Cmbbx デモ)：遅延読み込み化

--- _demo/ComboboxDemo.tsx
+++ _demo/ComboboxDemo.tsx
@@ -1,23 +1,5 @@
 import { useState } from "preact/hooks"
 
-import IconCheck from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/check.tsx"
-import IconBracketsAngle from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/brackets-angle.tsx"
-
-import { Button } from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx"
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/command.tsx"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/popover.tsx"
-
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -105,47 +87,70 @@ const frameworks = [ { value: "next.js", label: "Next.js" },
 
 
 export function ComboboxDemo() {
-  const [open, setOpen] = useState(false)
-  const [value, setValue] = useState("")
+  const createComp = async () => {
+    const { default: IconCheck } = await import("https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/check.tsx")
+    const { default: IconBracketsAngle } = await import("https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/brackets-angle.tsx")
+    const { Button } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/button.tsx")
+    const {
+      Command,
+      CommandEmpty,
+      CommandGroup,
+      CommandInput,
+      CommandItem,
+    } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/command.tsx")
+    const {
+      Popover,
+      PopoverContent,
+      PopoverTrigger,
+    } = await import("https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/popover.tsx")
+
+    const Demo = () => {
+      const [open, setOpen] = useState(false)
+      const [value, setValue] = useState("")
+      return(
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              class="w-[200px] justify-between"
+            >
+              {value
+                ? frameworks.find((framework) => framework.value === value)?.label
+                : "Select framework..."}
+              <IconBracketsAngle class="rotate-90 ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent class="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Search framework..." />
+              <CommandEmpty>No framework found.</CommandEmpty>
+              <CommandGroup>
+                {frameworks.map((framework) => (
+                  <CommandItem
+                    key={framework.value}
+                    value={framework.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue)
+                      setOpen(false)
+                    }}
+                  >
+                    <IconCheck class={
+                      `mr-2 h-4 w-4 ${value === framework.value ? "opacity-100" : "opacity-0"}`}/>
+                    {framework.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      )
+    }
+    return Demo
+  }
+
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} >
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            class="w-[200px] justify-between"
-          >
-            {value
-              ? frameworks.find((framework) => framework.value === value)?.label
-              : "Select framework..."}
-            <IconBracketsAngle class="rotate-90 ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent class="w-[200px] p-0">
-          <Command>
-            <CommandInput placeholder="Search framework..." />
-            <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup>
-              {frameworks.map((framework) => (
-                <CommandItem
-                  key={framework.value}
-                  value={framework.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue)
-                    setOpen(false)
-                  }}
-                >
-                  <IconCheck class={
-                    `mr-2 h-4 w-4 ${value === framework.value ? "opacity-100" : "opacity-0"}`}/>
-                  {framework.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp} />
   )
 }
