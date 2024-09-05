2c59724774b462f78b146fe214260a8bb411b0ad
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-05 09:40:29 +0900

    変更(Acrd デモ)：遅延読み込み化

--- _demo/AccordionDemo.tsx
+++ _demo/AccordionDemo.tsx
@@ -1,10 +1,3 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger
-} from 'https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/accordion.tsx'
-
 import DemoWrapper from "./_DemoWrapper.tsx"
 
 
@@ -51,25 +44,39 @@ const has_error = {
 
 
 export function AccordionDemo() {
+  async function createComp() {
+    const {
+      Accordion,
+      AccordionContent,
+      AccordionItem,
+      AccordionTrigger
+    } = await import('https://deno.land/x/testing_shadcn_ui_for_deno@0.1.1/components/accordion.tsx')
+
+    const Demo = () => {
+      return(
+        <Accordion type="single" collapsible class="w-[500px]" >
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other components' aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      )
+    }
+    return Demo
+  }
+
   return (
-    <DemoWrapper code_text={Code.trim()} info={info} has_error={has_error}
-                preview_h={280} place_class="justify-center">
-      <Accordion type="single" collapsible class="w-[500px]" >
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Is it styled?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It comes with default styles that matches the other components' aesthetic.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>Is it animated?</AccordionTrigger>
-          <AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </DemoWrapper>
+    <DemoWrapper code_text={Code.trim()} info={info} funcCompForDemo={createComp}
+                has_error={has_error} preview_h={280} place_class="justify-center" />
   )
 }
