cc6c13a78a7b22a188d7181ba451a4357c011381
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-11 20:01:30 +0900

    index に記載していた header の要素を _app.tsx に移動

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,39 @@
-import { AppProps } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts"
 
-export default function App({ Component }: AppProps) {
+export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>testing-shadcn-in-fresh</title>
+        <link href={asset("/default.css")} rel="stylesheet" />
+        <link href={asset("/visual-studio-dark.css")} rel="stylesheet" />
+        <style>{`
+          :root{
+            --background: 0 0% 100%;
+            --foreground: 240 10% 3.9%;
+            --card: 0 0% 100%;
+            --card-foreground: 240 10% 3.9%;
+            --popover: 0 0% 100%;
+            --popover-foreground: 240 10% 3.9%;
+            --primary: 240 5.9% 10%;
+            --primary-foreground: 0 0% 98%;
+            --secondary: 240 4.8% 95.9%;
+            --secondary-foreground: 240 5.9% 10%;
+            --muted: 240 4.8% 95.9%;
+            --muted-foreground: 240 3.8% 46.1%;
+            --accent: 240 4.8% 95.9%;
+            --accent-foreground: 240 5.9% 10%;
+            --destructive: 0 72.22% 50.59%;
+            --destructive-foreground: 0 0% 98%;
+            --border: 240 5.9% 90%;
+            --input: 240 5.9% 90%;
+            --ring: 240 5% 64.9%;
+            --radius: 0.5rem
+          }
+      `}</style>
       </head>
       <body>
         <Component />
